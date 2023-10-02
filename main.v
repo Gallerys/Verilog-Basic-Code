01be091e381f498004a93e6f45c9a3c148c0a6ca
Code Gallery <59381987+Gallerys@users.noreply.github.com>
2023-10-02 21:52:25 +0600

    Update main.v

--- main.v
+++ main.v
@@ -1,6 +1,7 @@
 // This is a comment in Verilog. It's ignored by the Verilog compiler and used for documentation.
 
 // Define a module for a simple AND gate
+
 module AndGate(A, B, Y);
     input A, B;
     output Y;
